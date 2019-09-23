3aa5c66c40be9cf26268fe0573b32d6a390bdaf4
Newton Ni <nwtnni@gmail.com>
2019-09-23 17:13:00 -0400

    Add ALL_RE static; start filling out find

--- src/find.rs
+++ src/find.rs
@@ -6,9 +6,15 @@ const URL: &str = r"(?::https?://|git@|git://|ssh://|ftp://|file://)[^ ]+";
 lazy_static! {
     static ref SHA_RE: regex::Regex = regex::Regex::new(SHA).unwrap();
     static ref URL_RE: regex::Regex = regex::Regex::new(URL).unwrap();
+    static ref ALL_RE: [&'static regex::Regex; 2] = [&*SHA_RE, &*URL_RE];
     static ref SET_RE: regex::RegexSet = regex::RegexSet::new(&[SHA, URL]).unwrap();
 }
 
-pub fn matches(hay: &str) -> Vec<regex::Match> {
+pub fn matches(text: &str) -> Vec<regex::Match> {
+    for line in text.split('\n') {
+        for re in SET_RE.matches(text).iter().map(|index| ALL_RE[index]) {
+            // Individual scan
+        }
+    }
     unimplemented!()
 }
