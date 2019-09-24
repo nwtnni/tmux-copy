2fb227198ac124f52084a8c45e2ab6f1beddcf78
Newton Ni <nwtnni@gmail.com>
2019-09-24 11:40:19 -0400

    Avoid extra allocation from trim

--- src/tmux.rs
+++ src/tmux.rs
@@ -6,12 +6,11 @@ pub fn active() -> Result<String, Box<dyn error::Error>> {
         .output()
         .map(|out| out.stdout)
         .map(String::from_utf8)?
-        .map(|pane| pane.trim().to_owned())
         .map_err(From::from)
 }
 
 pub fn capture(pane: &str) -> Result<String, Box<dyn error::Error>> {
-    command!("tmux", "capture-pane", "-Jpt", pane)
+    command!("tmux", "capture-pane", "-Jpt", pane.trim())
         .output()
         .map(|out| out.stdout)
         .map(String::from_utf8)?
