f6a267a2371c73c327ed2aa7a3a4ff76f40a58e9
Newton Ni <nwtnni@gmail.com>
2019-09-24 11:32:17 -0400

    Fix URL regex

--- src/find.rs
+++ src/find.rs
@@ -1,7 +1,7 @@
 use lazy_static::lazy_static;
 
 const SHA: &str = r"[0-9a-f]{7, 40}";
-const URL: &str = r"(?::https?://|git@|git://|ssh://|ftp://|file://)[^ ]+";
+const URL: &str = r"(?:https?://|git@|git://|ssh://|ftp://|file://)[^ ]+";
 
 lazy_static! {
     static ref SHA_RE: regex::Regex = regex::Regex::new(SHA).unwrap();
