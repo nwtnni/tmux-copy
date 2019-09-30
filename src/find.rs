1cd90b63b8a00722ba1f5b4cd4f921a4c7aaed0f
Newton Ni <nwtnni@gmail.com>
2019-09-30 10:54:53 -0400

    Add IP, UID regexes

--- src/find.rs
+++ src/find.rs
@@ -1,15 +1,21 @@
 use lazy_static::lazy_static;
 
+const IPV4: &str = r"[[:digit:]]{1,3}\.[[:digit:]]{1,3}\.[[:digit:]]{1,3}\.[[:digit:]]{1,3}";
+const IPV6: &str = r"[[:xdigit:]]+:+[[:xdigit:]]+[^[[:space:]]]+";
 const PATH: &str = r"/?(?:(?:[[:word:]]|-|~|\.)+/)+(?:[[:word:]]|-|~|\.)*";
-const SHA: &str = r"[0-9a-f]{7, 40}";
-const URL: &str = r"(?:https?://|git@|git://|ssh://|ftp://|file://)[^ ]+";
+const SHA: &str = r"[[:xdigit:]]{7, 40}";
+const URL: &str = r"(?:https?://|git@|git://|ssh://|ftp://|file://)[^[[:space:]]]+";
+const UID: &str = r"[[:xdigit:]]{8}-[[:xdigit:]]{4}-[[:xdigit:]]{4}-[[:xdigit:]]{4}-[[:xdigit:]]{12}";
 
 lazy_static! {
+    static ref IPV4_RE: regex::Regex = regex::Regex::new(IPV4).unwrap();
+    static ref IPV6_RE: regex::Regex = regex::Regex::new(IPV6).unwrap();
     static ref PATH_RE: regex::Regex = regex::Regex::new(PATH).unwrap();
     static ref SHA_RE: regex::Regex = regex::Regex::new(SHA).unwrap();
     static ref URL_RE: regex::Regex = regex::Regex::new(URL).unwrap();
-    static ref ALL_RE: [&'static regex::Regex; 3] = [&*PATH_RE, &*SHA_RE, &*URL_RE];
-    static ref SET_RE: regex::RegexSet = regex::RegexSet::new(&[PATH, SHA, URL]).unwrap();
+    static ref UID_RE: regex::Regex = regex::Regex::new(UID).unwrap();
+    static ref ALL_RE: [&'static regex::Regex; 6] = [&*IPV4_RE, &*IPV6_RE, &*PATH_RE, &*SHA_RE, &*URL_RE, &*UID_RE];
+    static ref SET_RE: regex::RegexSet = regex::RegexSet::new(&[IPV4, IPV6, PATH, SHA, URL]).unwrap();
 }
 
 #[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord)]
