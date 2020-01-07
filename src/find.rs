943658a09a71a86709b63eb196fa9bfb33a84a25
Newton Ni <nwtnni@gmail.com>
2020-01-07 10:35:19 -0500

    Support capture groups and Markdown inline code

--- src/find.rs
+++ src/find.rs
@@ -17,10 +17,11 @@ regex_set! {
     IPV4: r"[[:digit:]]{1,3}\.[[:digit:]]{1,3}\.[[:digit:]]{1,3}\.[[:digit:]]{1,3}";
     IPV6: r"[[:xdigit:]]+:+[[:xdigit:]]+[^[[:space:]]]+";
     KUBE: r"[a-z0-9]+(?:-[a-z0-9]+)+";
-    PATH: r"/?([[[:word:]]-~\.]+/)+[[[:word:]]-\.]*";
+    PATH: r"/?(?:[[[:word:]]-~\.]+/)+[[[:word:]]-\.]*";
     SHA:  r"[[:xdigit:]]{7, 40}";
     UID:  r"[[:xdigit:]]{8}-[[:xdigit:]]{4}-[[:xdigit:]]{4}-[[:xdigit:]]{4}-[[:xdigit:]]{12}";
     URL:  r#"(?:https?://|git@|git://|ssh://|ftp://|file://)[^'"\]\)[[:space:]]]+"#;
+    MD:   r"`([^`]+)`";
 }
 
 #[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord)]
@@ -34,20 +35,46 @@ pub fn matches(text: &str) -> Vec<Match> {
     let mut matches = Vec::new();
     for (row, line) in text.split('\n').enumerate() {
         let ascii = &util::Lazy::new(|| line.is_ascii());
+
+        /// Constructs a `Match` from a `regex::Match`.
+        /// We use a macro here to (a) avoid the overhead of
+        /// constructing a possibly unnecessary closure every
+        /// iteration, and (b) avoid having to name the
+        /// `util::Lazy<F>` type in a function parameter.
+        macro_rules! found {
+          ($match:expr) => {
+            {
+              let start = $match.start();
+              Match {
+                row: row as u16,
+                col: if ascii.force() { start } else { column(line, start) } as u16,
+                txt: $match.as_str(),
+              }
+            }
+          }
+        }
+
         matches.extend(
             SET_RE.matches(text)
                 .iter()
                 .map(|index| ALL_RE[index])
-                .flat_map(move |re| re.find_iter(line).map(move |r#match| {
-                    let start = r#match.start();
-                    Match {
-                        row: row as u16,
-                        col: if ascii.force() { start } else { column(line, start) } as u16,
-                        txt: r#match.as_str(),
-                    }
-                }))
+                .flat_map(move |re| {
+                  if re.captures_len() == 1 {
+                    util::Or::L(
+                      re.find_iter(line)
+                        .map(move |r#match| found!(r#match))
+                    )
+                  } else {
+                    util::Or::R(
+                      re.captures_iter(line)
+                        .filter_map(|capture| capture.get(1))
+                        .map(move |r#match| found!(r#match))
+                    )
+                  }
+                })
         );
     }
+
     matches.sort();
     matches
 }
